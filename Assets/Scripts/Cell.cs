526c22d91f8172a78f33ae6c17715e35ab5b6ae7
will b. gaming <wiljenki@gmail.com>
2026-02-15 22:49:32 -0500

    i did the algorithm in one day??

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,20 +1,58 @@
 using TMPro;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
-    [SerializeField] private GameObject text;
-    private int value; // 0 if not locked
-    private Vector2Int coordinate;
+    [SerializeField] private GameObject background, text;
+    private int value, guess; // value is 0 if not locked
+    private Vector2Int coordinate, box;
+    [SerializeField] private Color defaultColor, transparent, selected, defaultText, selectedText, penText;
+    private bool revealed = false;
 
+    public void SetColor(int val) // 0 = default, 1 = transparent, 2 = selected
+    {
+        if (val == 0)
+        {
+            background.GetComponent<Image>().color = defaultColor;
+            text.GetComponent<TMP_Text>().color = defaultText;  
+        }
+        else if (val == 1)
+        {
+            background.GetComponent<Image>().color = transparent;
+            text.GetComponent<TMP_Text>().color = selectedText;
+        }
+        else
+        {
+            background.GetComponent<Image>().color = selected;
+            text.GetComponent<TMP_Text>().color = selectedText;
+        }
+    }
     public void SetValue(int val)
     {
         value = val;
-        text.GetComponent<TMP_Text>().text = value.ToString();
     }
     public int GetValue()
     {
         return value;
+    }
+    public void SetRevealed(bool val)
+    {
+        revealed = val;
+    }
+    public bool GetRevealed()
+    {
+        return revealed;
+    }
+    public void SetGuess(int val)
+    {
+        guess = val;
+        text.GetComponent<TMP_Text>().text = val.ToString();
+    }
+    public int GetGuess()
+    {
+        return guess;
     }
     public void SetCoordinate(Vector2Int val)
     {
@@ -23,5 +61,13 @@ public class Cell : MonoBehaviour
     public Vector2Int GetCoordinate()
     {
         return coordinate;
+    }
+    public void SetBox(Vector2Int val)
+    {
+        box = val;
+    }
+    public Vector2Int GetBox()
+    {
+        return box;
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,3 +1,7 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
@@ -7,10 +11,37 @@ public class Game : MonoBehaviour
     private const int SIZE = 9;
     private Cell[,] cells = new Cell[SIZE,SIZE];
     [SerializeField] private GameObject cellPrefab, cellBoard;
+    private Cell selection;
+    private bool selected = false;
     void Start()
     {
         AssignArray();
         GenerateBoard();
+        ChangeSelection(cells[4, 0]);
+    }
+    private void ChangeSelection(Cell cell, bool voluntary = false)
+    {
+        if (voluntary)
+        {
+            selected = true;
+        }
+        selection = cell;
+        List<Cell> targetCells = GetTargetCells(cell);
+        for (int row = 0; row<SIZE; row++)
+        {
+            for (int column = 0; column<SIZE; column++)
+            {
+                if (targetCells.Contains(cells[column, row]))
+                {
+                    cells[column, row].SetColor(1);
+                }
+                else
+                {
+                    cells[column, row].SetColor(0);
+                }
+            }
+        }
+        cell.SetColor(2);
     }
     private void AssignArray()
     {
@@ -25,7 +56,7 @@ public class Game : MonoBehaviour
                     {
                         cells[column + boxColumn * 3, row + boxRow * 3] = box.GetChild(row*3 + column).GetComponent<Cell>();
                         cells[column + boxColumn * 3, row + boxRow * 3].SetCoordinate(new Vector2Int(column + boxColumn * 3, row + boxRow * 3));
-                        cells[column + boxColumn * 3, row + boxRow * 3].SetValue(5);
+                        cells[column + boxColumn * 3, row + boxRow * 3].SetBox(new Vector2Int(boxColumn, boxRow));
                     }
                 }
             }
@@ -33,6 +64,187 @@ public class Game : MonoBehaviour
     }
     private void GenerateBoard()
     {
-
+        NextStep();
+    }
+    // Returns false if the step results in a contradiction.
+    private bool NextStep()
+    {
+        if (!BoardNotFinished())
+        {
+            return true;
+        }
+        Cell target = GetMostSolved();
+        List<int> potentialAnswers = GetPotentialAnswers(target);
+        for (int i = 0; i < potentialAnswers.Count; i++)
+        {
+            target.SetValue(potentialAnswers[i]);
+            if (NextStep())
+            {
+                return true;
+            }
+        }
+        // This step has run out of valid numbers, meaning there is a contradiction
+        target.SetValue(0);
+        return false;
+    }
+    private bool BoardNotFinished()
+    {
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetValue() == 0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    // Returns the cell with the lowest amount of potential answers.
+    private Cell GetMostSolved()
+    {
+        List<Cell> returnList = new List<Cell>();
+        int leastAnswers = 9;
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetValue() != 0)
+                {
+                    continue;
+                }
+                int potentialAnswers = CountPotentialAnswers(cells[column, row]);
+                if (potentialAnswers >= leastAnswers)
+                {
+                    continue;
+                }
+                leastAnswers = potentialAnswers;
+            }
+        }
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetValue() != 0)
+                {
+                    continue;
+                }
+                int potentialAnswers = CountPotentialAnswers(cells[column, row]);
+                if (potentialAnswers > leastAnswers)
+                {
+                    continue;
+                }
+                returnList.Add(cells[column, row]);
+            }
+        }
+        return returnList[Random.Range(0, returnList.Count)];
+    }
+    private List<int> GetPotentialAnswers(Cell cell)
+    {
+        if (cell.GetValue() != 0)
+        {
+            return new List<int>();
+        }
+        List<int> answers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<Cell> targetCells = GetTargetCells(cell);
+        for (int i = 0; i < targetCells.Count; i++)
+        {
+            if (!answers.Contains(targetCells[i].GetValue()))
+            {
+                continue;
+            }
+            answers.Remove(targetCells[i].GetValue());
+        }
+        return answers;
+    }
+    private int CountPotentialAnswers(Cell cell)
+    {
+        if (cell.GetValue() != 0)
+        {
+            return 0;
+        }
+        List<int> answers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        List<Cell> targetCells = GetTargetCells(cell);
+        for (int i = 0; i < targetCells.Count; i++)
+        {
+            if (!answers.Contains(targetCells[i].GetValue()))
+            {
+                continue;
+            }
+            answers.Remove(targetCells[i].GetValue());
+        }
+        return answers.Count;
+    }
+    private List<Cell> GetTargetCells(Cell cell)
+    {
+        List<Cell> targets = new List<Cell>();
+        targets = AddNew(targets, GetTargetColumn(cell));
+        targets = AddNew(targets, GetTargetRow(cell));
+        targets = AddNew(targets, GetTargetBox(cell));
+        return targets;
+    }
+    private List<Cell> AddNew(List<Cell> mainList, List<Cell> list2)
+    {
+        for(int i = 0; i < list2.Count; i++)
+        {
+            if (mainList.Contains(list2[i]))
+            {
+                continue;
+            }
+            mainList.Add(list2[i]);
+        }
+        return mainList;
+    }
+    private List<Cell> GetTargetColumn(Cell cell)
+    {
+        List<Cell> targets = new List<Cell>();
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetCoordinate().x != cell.GetCoordinate().x)
+                {
+                    continue;
+                }
+                targets.Add(cells[column, row]);
+            }
+        }
+        targets.Remove(cell);
+        return targets;
+    }
+    private List<Cell> GetTargetRow(Cell cell)
+    {
+        List<Cell> targets = new List<Cell>();
+        for (int row = 0; row < SIZE; row++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetCoordinate().y != cell.GetCoordinate().y)
+                {
+                    continue;
+                }
+                targets.Add(cells[column, row]);
+            }
+        }
+        targets.Remove(cell);
+        return targets;
+    }
+    private List<Cell> GetTargetBox(Cell cell)
+    {
+        List<Cell> targets = new List<Cell>();
+        for (int row = 0; row < SIZE; row ++)
+        {
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (cells[column, row].GetBox() != cell.GetBox())
+                {
+                    continue;
+                }
+                targets.Add(cells[column, row]);
+            }
+        }
+        targets.Remove(cell);
+        return targets;
     }
 }
